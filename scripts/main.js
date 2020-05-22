5cf6e96d08de7ef4a34a5f19478b7fb39481776f
zainbits <hello.zain11@gmail.com>
2020-05-22 18:22:05 +0530

    Stage-1.3 | Added Modals & Plugged Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  });
+// PAGE RELATED JAVASCRIPTS
+
+document.addEventListener('DOMContentLoaded', function () {
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+  
+  var collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
+
+
+// FIREBASE RELATED JAVASCRIPTS
+
