ab0fc0bcc8ee59ee841ee84e6802152a03a1626f
zainbits <hello.zain11@gmail.com>
2020-05-22 18:39:55 +0530

    Stage-2.0 | Create Account Firebase Module Done

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
-  
+
   var collapsibles = document.querySelectorAll('.collapsible');
   M.Collapsible.init(collapsibles);
 });
@@ -11,3 +11,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // FIREBASE RELATED JAVASCRIPTS
 
+const createaccountForm = document.querySelector('#createaccount-form');
+createaccountForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //Get USER Info
+  const email = createaccountForm['create-email'].value;
+  const pwd = createaccountForm['create-pwd'].value;
+
+  //Create Account
+  auth.createUserWithEmailAndPassword(email, pwd).then(tok => {
+    console.log(tok.user);
+    const modal = document.querySelector('#mod-createaccount');
+    M.Modal.getInstance(modal).close();
+    createaccountForm.reset();
+  })
+
+})
